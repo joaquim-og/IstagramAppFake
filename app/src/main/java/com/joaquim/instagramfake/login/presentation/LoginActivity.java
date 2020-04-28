8aeffaa16fc15ba65d1f5147df91a86844144c0d
joaquim_og@hotmail.com <virgem89>
2020-04-28 17:41:42 -0300

    UX login page

--- app/src/main/java/com/joaquim/instagramfake/login/presentation/LoginActivity.java
+++ app/src/main/java/com/joaquim/instagramfake/login/presentation/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.joaquim.instagramfake;
+package com.joaquim.instagramfake.login.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.joaquim.instagramfake.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
