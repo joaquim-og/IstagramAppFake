299f91740bbc42ad46af9720e403cb4d5f6fe3da
joaquim_og@hotmail.com <virgem89>
2020-05-07 19:03:07 -0300

    adjusts in profile grid

--- app/src/main/java/main/profile/presentation/ProfileFragment.java
+++ app/src/main/java/main/profile/presentation/ProfileFragment.java
@@ -4,26 +4,81 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.joaquim.instagramfake.R;
 
 public class ProfileFragment extends Fragment {
 
+    public ProfileFragment() {}
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_main_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_profile, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.profile_recycler);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerView.setAdapter(new PostAdaper());
+
+
+        return view;
     }
 
-    public ProfileFragment(){
+    private class PostAdaper extends RecyclerView.Adapter<PostViewHolder> {
 
+        private int[] images = new int[]{
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
 
+        };
 
+        @NonNull
+        @Override
+        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+            return new PostViewHolder(getLayoutInflater().inflate(R.layout.item_profile_grid, viewGroup, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PostViewHolder postViewHolder, int i) {
+            postViewHolder.bind(images[i]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return images.length;
+        }
     }
 
+
+    private static class PostViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imagePost;
+
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imagePost = itemView.findViewById(R.id.profileImage_grid);
+        }
+
+        public void bind(int image) {
+            this.imagePost.setImageResource(image);
+        }
+    }
 }
