7209f541fb5af4d10f754d340167bce82b339f71
joaquim_og@hotmail.com <virgem89>
2020-05-12 17:17:17 -0300

    built UI camera and gallery

---
+++ app/src/main/java/main/search/presentation/SearchFragment.java
@@ -0,0 +1,98 @@
+package main.search.presentation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.joaquim.instagramfake.R;
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {}
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main_search, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.search_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new PostAdaper());
+
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_profile, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private class PostAdaper extends RecyclerView.Adapter<PostViewHolder> {
+
+        private int[] images = new int[]{
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+            R.drawable.ic_insta_add,
+
+        };
+
+        @NonNull
+        @Override
+        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+            return new PostViewHolder(getLayoutInflater().inflate(R.layout.item_user_list, viewGroup, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PostViewHolder postViewHolder, int i) {
+            postViewHolder.bind(images[i]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return images.length;
+        }
+    }
+
+
+    private static class PostViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imagePost;
+
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imagePost = itemView.findViewById(R.id.main_search_imageview_user);
+        }
+
+        public void bind(int image) {
+            this.imagePost.setImageResource(image);
+        }
+    }
+}
